be01f27a1bbfee2835f9624f6a58b3cf3385b351
kradworkz <80160702+kradworkz@users.noreply.github.com>
2023-05-18 16:39:55 +0800

    asd

--- app/Http/Controllers/Scholar/FinancialBenefit/IndexController.php
+++ app/Http/Controllers/Scholar/FinancialBenefit/IndexController.php
@@ -141,7 +141,7 @@ class IndexController extends Controller
             'user' => $user
         ];
 
-        $pdf = \PDF::loadView('Prints.FinancialBenefits',$array)->setPaper('a4', 'portrait');
+        $pdf = \PDF::loadView('prints.financialbenefits',$array)->setPaper('a4', 'portrait');
         return $pdf->download('FinancialBenefit.pdf');
     }
 
@@ -165,7 +165,7 @@ class IndexController extends Controller
 
         foreach($lists as $list){
             foreach($privileges as $privilege){
-
+                
             }
             $arr = [
                 'ay' => $list->academic_year.' - '.$list->semester->name,
